1cd68e61b43b189336dcb472277a53cc18a8a78a
winterchen <1085143002@qq.com>
2020-02-28 14:15:16 +0800

    添加方法的出入参和耗时切面打印

--- log-trace-spring-boot-starter/src/main/java/com/purgerteam/log/trace/starter/filter/TraceFilter.java
+++ log-trace-spring-boot-starter/src/main/java/com/purgerteam/log/trace/starter/filter/TraceFilter.java
@@ -2,18 +2,16 @@ package com.purgerteam.log.trace.starter.filter;
 
 import com.purgerteam.log.trace.starter.Constants;
 import com.purgerteam.log.trace.starter.TraceContentFactory;
-import com.purgerteam.log.trace.starter.wrapper.HeaderMapRequestWrapper;
-import org.apache.tomcat.util.http.MimeHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,7 +26,6 @@ public class TraceFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
 
-        //HeaderMapRequestWrapper requestWrapper = new HeaderMapRequestWrapper(httpServletRequest);
         String headerTraceId = httpServletRequest.getHeader(Constants.LEGACY_TRACE_ID_NAME);
         String headerParentServiceName = httpServletRequest.getHeader(Constants.LEGACY_PARENT_SERVICE_NAME);
 
--- log-trace-spring-boot-starter/src/main/java/com/purgerteam/log/trace/starter/instrument/hystrix/HystrixAutoConfiguration.java
+++ log-trace-spring-boot-starter/src/main/java/com/purgerteam/log/trace/starter/instrument/hystrix/HystrixAutoConfiguration.java
@@ -1,11 +1,14 @@
 package com.purgerteam.log.trace.starter.instrument.hystrix;
 
 import com.netflix.hystrix.strategy.HystrixPlugins;
+import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
+@ConditionalOnClass(value = {HystrixConcurrencyStrategy.class})
 public class HystrixAutoConfiguration {
 
     @PostConstruct
